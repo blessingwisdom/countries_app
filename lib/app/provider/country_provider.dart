1ac351e3bfd18d8d4e47ca3a8e0d916bd175034e
Blessing Wisdom <88656234+blessingwisdom@users.noreply.github.com>
2022-11-12 17:09:08 +0100

    details screen

--- lib/app/provider/country_provider.dart
+++ lib/app/provider/country_provider.dart
@@ -1,1 +1,40 @@
+import 'package:countries_app/app/models/countries.dart';
+import 'package:countries_app/app/services/countries_api.dart';
+import 'package:flutter/cupertino.dart';
 
+class CountriesProvider with ChangeNotifier {
+  late List<CountriesModel>? _countryList = [];
+  late List<CountriesModel>? _countryByNameList = [];
+  late List<CountriesModel>? _searchResult = [];
+  late List<CountriesModel>? _finalList = [];
+  CountriesApi countriesApiService = CountriesApi();
+  bool _isLoading = true;
+  get countryData => _searchResult!.isEmpty ? _countryList : _searchResult;
+  get countryByNameList => _countryByNameList;
+  get isLoading => _isLoading;
+
+  getAllCountriesData() async {
+    _countryList = (await countriesApiService.getAllCountries());
+    _countryList!.sort((a, b) => a.name.common
+        .toLowerCase()
+        .compareTo(b.name.common.toLowerCase().toString()));
+    _isLoading = false;
+    notifyListeners();
+  }
+
+  getAllCountryByName(String name) async {
+    _countryByNameList = (await countriesApiService.getCountryBName(name));
+
+    notifyListeners();
+  }
+
+  searchResult(String text) {
+    _searchResult!.clear();
+    for (var value in _countryList!) {
+      if (value.name.common.toLowerCase().contains(text.toLowerCase())) {
+        _searchResult!.add(value);
+      }
+    }
+    notifyListeners();
+  }
+}
---
+++ lib/app/views/details_screen.dart
@@ -0,0 +1,174 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:countries_app/app/widget/details_widget.dart';
+import 'package:provider/provider.dart';
+import 'package:countries_app/app/provider/theme_provider.dart';
+import 'package:countries_app/app/provider/country_provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:card_swiper/card_swiper.dart';
+
+class DetailsScreen extends StatefulWidget {
+  const DetailsScreen({Key? key, required this.name}) : super(key: key);
+  final String name;
+
+  @override
+  State<DetailsScreen> createState() => _DetailsScreenState();
+}
+
+class _DetailsScreenState extends State<DetailsScreen> {
+  @override
+  void initState() {
+    final _Countiesrover =
+        Provider.of<CountriesProvider>(context, listen: false);
+    _Countiesrover.getAllCountryByName(widget.name);
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        margin: EdgeInsets.only(left: 24.w, right: 24.w, top: 24.h),
+        child: Consumer<DarkThemeProvider>(
+            builder: (BuildContext context, data, child) {
+          return Consumer<CountriesProvider>(
+              builder: (BuildContext context, countriesDataByName, child) {
+            return Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  height: 24.h,
+                ),
+                SizedBox(
+                  height: 33.h,
+                  width: 214.w,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pop(context);
+                        },
+                        child: Icon(Icons.arrow_back),
+                      ),
+                      Text(
+                        countriesDataByName.countryByNameList.length > 0
+                            ? countriesDataByName
+                                .countryByNameList[0].name.common
+                                .toString()
+                            : "no data",
+                        style: GoogleFonts.firaSans(fontSize: 18.sp),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 200.h,
+                  width: double.maxFinite,
+                  child: Swiper(
+                    itemBuilder: (BuildContext context, int index) {
+                      return Container(
+                        decoration: BoxDecoration(
+                            color: Colors.grey.shade100,
+                            borderRadius: BorderRadius.circular(5),
+                            image: DecorationImage(
+                                image: NetworkImage(
+                                  countriesDataByName.countryByNameList.length >
+                                          0
+                                      ? countriesDataByName
+                                          .countryByNameList[0].flags.png
+                                          .toString()
+                                      : "no data",
+                                ),
+                                fit: BoxFit.cover)),
+                      );
+                    },
+                    autoplay: false,
+                    itemCount: 3,
+                    pagination: const SwiperPagination(
+                        alignment: Alignment.bottomCenter,
+                        builder: DotSwiperPaginationBuilder(
+                            color: Colors.white,
+                            activeColor: Colors.blue,
+                            size: 5,
+                            activeSize: 5)),
+                    control: SwiperControl(
+                        iconPrevious: Icons.arrow_back_ios,
+                        iconNext: Icons.arrow_forward_ios,
+                        color: Colors.white,
+                        size: 20,
+                        padding: EdgeInsets.symmetric(horizontal: 20.w)),
+                  ),
+                ),
+                SizedBox(
+                  height: 18.h,
+                ),
+                DetailWidget(
+                  population: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].population
+                          .toString()
+                      : 'no data',
+                  region: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].region
+                          .toString()
+                      : 'no data',
+                  capital: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].capital
+                          .toString()
+                          .replaceAll("[", "")
+                          .replaceAll("]", "")
+                      : "no data",
+                  motto: "motto",
+                  officialLang: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName
+                          .countryByNameList[0].languages.values.first
+                          .toString()
+                      : "no data",
+                  ethnicGroup: "ethnicGroup",
+                  religion: "religion",
+                  govt: "govt",
+                  independence: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].independent
+                          .toString()
+                      : "no data",
+                  area: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].area
+                              .toString() +
+                          "km2"
+                      : "no data",
+                  currency: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName
+                          .countryByNameList[0].currencies.values.first["name"]
+                          .toString()
+                      : 'no data',
+                  gdp: 'gdp',
+                  timeZone: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].timezones[0]
+                          .toString()
+                      : "no data",
+                  dateFormat: 'dateFormat',
+                  dialingCode: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].idd['root']
+                              .toString()
+                              .replaceAll("[", "")
+                              .replaceAll("]", "") +
+                          countriesDataByName
+                              .countryByNameList[0].idd['suffixes']
+                              .toString()
+                              .replaceAll("[", "")
+                              .replaceAll("]", "")
+                      : "no data",
+                  drivingSide: countriesDataByName.countryByNameList.length > 0
+                      ? countriesDataByName.countryByNameList[0].car['side']
+                          .toString()
+                      : 'no data',
+                )
+              ],
+            );
+          });
+        }),
+      ),
+    );
+  }
+}
